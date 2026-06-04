e6108750983de85efb22b5e0a6bb24929f0faf3c
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-04 17:49:06 +0530

    complete the work

--- React/Day 11 - React Router/vite-project/src/App.jsx
+++ React/Day 11 - React Router/vite-project/src/App.jsx
@@ -4,28 +4,22 @@ import Home from "./pages/Home";
 import Users from "./pages/Users";
 import AddUser from "./pages/AddUser";
 import EditUser from "./pages/EditUser";
-import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
+import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <>
-      <Navbar />
-      
-        <div className="w-xl-7 mx-auto p-2">
- <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/users" element={<Users />} />
-
-        <Route path="/add-user" element={<AddUser />} />
-
-        <Route path="/edit-user/:id" element={<EditUser />} />
-        <Route path="*" element ={<NotFound/>}/>
-
-      </Routes>
-        </div>
-     
+      <div className="w-xl-7 mx-auto p-2">
+        <Routes>
+          <Route path="/" element={<MainLayout/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="/edit-user/:id" element={<EditUser />} />
+          <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
