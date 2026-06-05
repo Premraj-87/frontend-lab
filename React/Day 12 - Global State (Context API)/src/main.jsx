745df0534af047ba92fcea2561046fe500f134aa
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-05 08:52:18 +0530

    understand about useContext

--- React/Day 12 - Global State (Context API)/src/main.jsx
+++ React/Day 12 - Global State (Context API)/src/main.jsx
@@ -1,12 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { NotesProvider } from "./context/NotesContext.jsx";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from './context/ThemeContext.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
+createRoot(document.getElementById("root")).render(
+  <NotesProvider>
     <App />
-  </ThemeProvider>
-
-)
+  </NotesProvider>,
+);
