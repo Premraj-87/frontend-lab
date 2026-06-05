78cfe66523516e990d82b2a7a3554f25a112648c
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-05 13:13:00 +0530

    new topic started

--- React/Day 12 - Global State (Context API)/src/context/NotesContext.jsx
+++ React/Day 12 - Global State (Context API)/src/context/NotesContext.jsx
@@ -1,86 +1,49 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 // Create Context
 const NotesContext = createContext();
 
 // Provider
 export const NotesProvider = ({ children }) => {
-
   // Load Notes from LocalStorage
   const [notes, setNotes] = useState(() => {
-
     try {
+      const savedNotes = localStorage.getItem("notes");
 
-      const savedNotes =
-        localStorage.getItem("notes");
-
-      return savedNotes
-        ? JSON.parse(savedNotes)
-        : [];
-
+      return savedNotes ? JSON.parse(savedNotes) : [];
     } catch (error) {
-
-      console.error(
-        "Failed to load notes:",
-        error
-      );
+      console.error("Failed to load notes:", error);
 
       return [];
-
     }
-
   });
 
   // Add Note
   const addNote = (text) => {
-
     const trimmedText = text.trim();
 
     if (!trimmedText) return;
 
     const newNote = {
-
       id: crypto.randomUUID(),
 
       text: trimmedText,
-
     };
 
-    setNotes((prevNotes) => [
-      ...prevNotes,
-      newNote,
-    ]);
-
+    setNotes((prevNotes) => [...prevNotes, newNote]);
   };
 
   // Delete Note
   const deleteNote = (id) => {
-
-    setNotes((prevNotes) =>
-      prevNotes.filter(
-        (note) => note.id !== id
-      )
-    );
-
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
   };
 
   // Save Notes to LocalStorage
   useEffect(() => {
-
-    localStorage.setItem(
-      "notes",
-      JSON.stringify(notes)
-    );
-
+    localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
   return (
-
     <NotesContext.Provider
       value={{
         notes,
@@ -88,30 +51,19 @@ export const NotesProvider = ({ children }) => {
         deleteNote,
       }}
     >
-
       {children}
-
     </NotesContext.Provider>
-
   );
-
 };
 
 // Custom Hook
 // eslint-disable-next-line react-refresh/only-export-components
 export const useNotes = () => {
-
-  const context =
-    useContext(NotesContext);
+  const context = useContext(NotesContext);
 
   if (!context) {
-
-    throw new Error(
-      "useNotes must be used inside NotesProvider"
-    );
-
+    throw new Error("useNotes must be used inside NotesProvider");
   }
 
   return context;
-
 };
