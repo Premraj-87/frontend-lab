2a9df0cd14cd3799811cc3fa8dbbcb1eb4ba7115
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-06 12:21:50 +0530

    ok done

--- React/Day 14 - UI UX Enhancements/vite-project/src/layouts/MainLayout.jsx
+++ React/Day 14 - UI UX Enhancements/vite-project/src/layouts/MainLayout.jsx
@@ -1,10 +1,17 @@
 const MainLayout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100">
-      <header className="bg-black text-white p-4">
-        <h1 className="text-2xl font-bold">🎥 CineValut</h1>
+      <header className="bg-black text-white p-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold flex flex-col">
+          <span>🎬 CineVault</span>
+          <span className="text-sm font-normal">Discover Movies</span>
+        </h1>
+        <nav className="space-x-4">
+          <a href="#" className="hover:underline">Home</a>
+          <a href="#" className="hover:underline">Favorites</a>
+        </nav>
       </header>
-      <main className="max-w-7xl mx-auto p-4">
+      <main className="max-w-6xl mx-auto p-4">
         {children}
       </main>
     </div>
