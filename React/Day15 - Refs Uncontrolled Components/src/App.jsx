e79e1f2d477569cd95a4d7eca7b7e69a3aa7b7b6
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-07 16:29:13 +0530

    updated

--- React/Day15 - Refs Uncontrolled Components/src/App.jsx
+++ React/Day15 - Refs Uncontrolled Components/src/App.jsx
@@ -1,13 +1,64 @@
-function App() {
+import { useRef } from "react";
 
-  const goTop = () => {
-    window.scrollTo(0, 0);
+
+function App() {
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({
+      behavior: "smooth",
+    });
   };
 
   return (
-    <button onClick={goTop}>
-      Top
-    </button>
+    <>
+      <nav>
+        <button onClick={() => scrollToSection(homeRef)}>
+          Home
+        </button>
+
+        <button onClick={() => scrollToSection(aboutRef)}>
+          About
+        </button>
+
+        <button onClick={() => scrollToSection(skillsRef)}>
+          Skills
+        </button>
+
+        <button onClick={() => scrollToSection(projectsRef)}>
+          Projects
+        </button>
+
+        <button onClick={() => scrollToSection(contactRef)}>
+          Contact
+        </button>
+      </nav>
+
+      <section ref={homeRef}>
+        Home
+      </section>
+
+      <section ref={aboutRef}>
+        About
+      </section>
+
+      <section ref={skillsRef}>
+        Skills
+      </section>
+
+      <section ref={projectsRef}>
+        Projects
+      </section>
+
+      <section ref={contactRef}>
+        Contact
+      </section>
+    </>
   );
 }
-export default App
+
+export default App;
