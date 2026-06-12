0689f6fb5b8fec671afc4c40f284c41805044f7a
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-12 12:59:22 +0530

    created analyticsData

---
+++ React/Day 18 -Charts  Analytics/charts and bars/src/data/analyticsData.js
@@ -0,0 +1,20 @@
+export const todoStats = {
+  total: 50,
+  completed: 35,
+  pending: 15,
+};
+
+export const monthlyTodos = [
+  { month: "Jan", todos: 20 },
+  { month: "Feb", todos: 35 },
+  { month: "Mar", todos: 50 },
+  { month: "Apr", todos: 40 },
+];
+
+export const weeklyProgress = [
+  { day: "Mon", completed: 2 },
+  { day: "Tue", completed: 5 },
+  { day: "Wed", completed: 8 },
+  { day: "Thu", completed: 6 },
+  { day: "Fri", completed: 10 },
+];
