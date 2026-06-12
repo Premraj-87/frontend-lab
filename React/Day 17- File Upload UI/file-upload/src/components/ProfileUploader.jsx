288ddc011e5cc3eb4e103747060ebf62cd53ddc0
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-12 12:36:00 +0530

    project completed

--- React/Day 17- File Upload UI/file-upload/src/components/ProfileUploader.jsx
+++ React/Day 17- File Upload UI/file-upload/src/components/ProfileUploader.jsx
@@ -1,53 +1,38 @@
 import { useState } from "react";
 
 const ProfileUploader = () => {
-
   const [file, setFile] = useState(null);
   const [preview, setPreview] = useState("");
 
   const handleFileChange = (e) => {
-
     const selectedFile = e.target.files[0];
 
     if (!selectedFile) return;
 
     setFile(selectedFile);
 
-    const imageUrl =
-      URL.createObjectURL(selectedFile);
+    const imageUrl = URL.createObjectURL(selectedFile);
 
     setPreview(imageUrl);
   };
   const removeFile = () => {
-  setFile(null);
-  setPreview("");
-};
+    setFile(null);
+    setPreview("");
+  };
 
   return (
     <div>
-
       <h1>Profile Picture Upload</h1>
 
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleFileChange}
-      />
+      <input type="file" accept="image/*" onChange={handleFileChange} />
 
       <br />
       <br />
 
-      {preview && (
-        <img
-          src={preview}
-          alt="preview"
-          width="250"
-        />
-      )}
+      {preview && <img src={preview} alt="preview" width="250" />}
 
       {file && (
         <div>
-
           <p>Name: {file.name}</p>
 
           <p>Type: {file.type}</p>
@@ -57,13 +42,9 @@ const ProfileUploader = () => {
             {(file.size / 1024).toFixed(2)}
             KB
           </p>
-
         </div>
       )}
-      <button onClick={removeFile}>
-  Remove Image
-</button>
-
+      <button onClick={removeFile}>Remove Image</button>
     </div>
   );
 };
