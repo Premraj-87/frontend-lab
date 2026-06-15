6c832bd150e0ea6b7398c73a85d608a7ca3f2738
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-15 08:13:13 +0530

    Starting the topic routing in next js

---
+++ Next Js/basic/my-app/app/projects/page.js
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div>
+        <h1>This is Project Page</h1>
+    </div>
+  )
+}
+export default page
---
+++ Next Js/basic/my-app/app/services/page.js
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div><h1>This is Service Page</h1></div>
+  )
+}
+export default page
